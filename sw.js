4981744e90e890b74bd1a7945308af84d1e0e885
ncalbet <ncalbet1@xtec.cat>
2026-05-04 18:43:57 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker instal·lat");
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
-});
